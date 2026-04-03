8f97eaf97bbc0b38593208cd5793923119795dfe
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-03 17:50:48 -0600

    add lazy loading for imgs in HomeHot.vue

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from './router'
 
 import '@/styles/common.scss'
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 // import { getCategory } from './apis/testAPI'
 // getCategory().then((res) => {
 //   console.log(res)
@@ -14,5 +16,18 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    console.log(el, binding.value)
+
+    useIntersectionObserver(el, ([{ isIntersecting }]) => {
+      console.log(isIntersecting)
+      if (isIntersecting) {
+        el.src = binding.value
+      }
+    })
+  },
+})
 
 app.mount('#app')
