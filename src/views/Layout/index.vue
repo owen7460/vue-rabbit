1c0eea184b9404d6a166110669364b260611fe8d
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-03 04:46:57 -0600

    finish layout-structure

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,4 +1,11 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
 <template>
-  <div>Layout page</div>
-  <RouterView></RouterView>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
