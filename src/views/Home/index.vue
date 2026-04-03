afceec911e36febd4ae703d256af48c99cf4952d
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-03 12:38:23 -0600

    create home components

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,16 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
+</script>
 <template>
-  <div>Home page</div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
