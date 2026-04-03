5a37ac1bfedff86ec58a2b26f49f95c83cd8b03e
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-03 04:36:17 -0600

    add  images and styles

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@/styles/common.scss'
+
 import { getCategory } from './apis/testAPI'
 getCategory().then((res) => {
   console.log(res)
