9501930a4e67f6a2ce9a2827db2904c751164222
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-03 18:28:37 -0600

    finsih HomeProduct

--- src/directives/index.js
+++ src/directives/index.js
@@ -5,10 +5,7 @@ export const lazyPlugin = {
   install(app) {
     app.directive('img-lazy', {
       mounted(el, binding) {
-        console.log(el, binding.value)
-
         const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
-          console.log(isIntersecting)
           if (isIntersecting) {
             el.src = binding.value
             stop()
