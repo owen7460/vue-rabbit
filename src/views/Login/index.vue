51143ad5ebd74afb448bc632ae0cef933e55902e
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-04 16:15:08 -0600

    create rule for login

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,4 +1,18 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const form = ref({
+  account: '',
+  password: '',
+})
+
+const rules = {
+  account: [{ required: true, message: 'username is empty', trigger: 'blur' }],
+  password: [
+    { required: true, message: 'password is required', trigger: 'blur' },
+    { min: 6, max: 14, message: 'password must be more than 6 less than 14', trigger: 'blur' },
+  ],
+}
+</script>
 <template>
   <div>
     <header class="login-header">
@@ -20,12 +34,18 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form
+              :model="form"
+              :rules="rules"
+              label-position="right"
+              label-width="60px"
+              status-icon
+            >
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large"> 我已同意隐私条款和服务条款 </el-checkbox>
