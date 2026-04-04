a1cd9060bb69826bc3b85afa92d5ea03abe49cfc
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-04 04:36:02 -0600

    create 2 ranks

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,13 +2,13 @@
 import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import DetailHot from './components/DetailHot.vue'
 
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
   const res = await getDetail(route.params.id)
   goods.value = res.result
-  console.log(goods.value)
 }
 
 onMounted(() => {
@@ -113,7 +113,7 @@ onMounted(() => {
               </div>
             </div>
             <!-- 24热榜+专题推荐 -->
-            <div class="goods-aside"></div>
+            <div class="goods-aside"><DetailHot></DetailHot> <DetailHot></DetailHot></div>
           </div>
         </div>
       </div>
