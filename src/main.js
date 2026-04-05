754a933dbf39543e56d9d80603f440f146ab2b58
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-05 12:37:04 -0600

    pinia persisted

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 import '@/styles/common.scss'
 
-import { useIntersectionObserver } from '@vueuse/core'
+// import { useIntersectionObserver } from '@vueuse/core'
 
 // import { getCategory } from './apis/testAPI'
 // getCategory().then((res) => {
@@ -17,8 +18,10 @@ import { lazyPlugin } from '@/directives'
 import { componentPlugin } from '@/components'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
