69a52ce99ba2e40160903e774691aee006498c42
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-05 13:21:54 -0600

    dealed with case of 401

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 // import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -23,7 +24,13 @@ httpInstance.interceptors.request.use(
 httpInstance.interceptors.response.use(
   (res) => res.data,
   (e) => {
+    const userStore = useUserStore()
     ElMessage({ type: 'warning', message: e.response.data.message })
+    if (e.response.status === 401) {
+      userStore.clearUserInfo()
+      router.push('/login')
+    }
+
     return Promise.reject(e)
   },
 )
