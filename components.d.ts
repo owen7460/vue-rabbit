4103822d1838f93a7169f13b79691f88dcd2fe8a
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-05 21:14:27 -0600

    create member

--- components.d.ts
+++ components.d.ts
@@ -23,6 +23,7 @@ declare module 'vue' {
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
+    ElPagination: typeof import('element-plus/es')['ElPagination']
     ElPopconfirm: typeof import('element-plus/es')['ElPopconfirm']
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import Payback from '@/views/Pay/Payback.vue'
+import Member from '@/views/Member/index.vue'
+import Userinfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -48,6 +51,17 @@ const router = createRouter({
         {
           path: 'payback',
           component: Payback,
+        },
+        {
+          path: 'member',
+          component: Member,
+          children: [
+            { path: 'user', component: Userinfo },
+            {
+              path: 'order',
+              component: UserOrder,
+            },
+          ],
         },
       ],
     },
