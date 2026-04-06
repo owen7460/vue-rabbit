f03dcc96c47d524f00cafcbe6bf206d080de65a1
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-05 18:43:28 -0600

    finish merge cart func

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -81,6 +81,7 @@ export const useCartStore = defineStore(
       selectedCount,
       selectedPrice,
       clearCart,
+      updateNewList,
     }
   },
   {
--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { loginAPI } from '@/apis/user'
 import { useCartStore } from './cartStore'
+import { mergeCartAPI } from '@/apis/cart'
 export const useUserStore = defineStore(
   'user',
   () => {
@@ -11,6 +12,20 @@ export const useUserStore = defineStore(
     const getUserInfo = async ({ account, password }) => {
       const res = await loginAPI({ account, password })
       userInfo.value = res.result
+
+      //merge cartlist
+      await mergeCartAPI(
+        cartStore.cartList.map((item) => {
+          return {
+            skuId: item.skuId,
+            selected: item.selected,
+            count: item.count,
+          }
+        }),
+      )
+
+      //get new cartlist
+      cartStore.updateNewList()
     }
     //clear user info action
     const clearUserInfo = () => {
