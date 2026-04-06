54c4031ce91806a980f93ba1fc97968ec98ef078
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-05 20:06:55 -0600

    finish order

--- src/apis/checkout.js
+++ src/apis/checkout.js
@@ -5,3 +5,13 @@ export const getCheckInfoAPI = () => {
     url: '/member/order/pre',
   })
 }
+
+//create order
+
+export const createOrderAPI = (data) => {
+  return request({
+    url: '/member/order',
+    method: 'POST',
+    data,
+  })
+}
--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,7 +1,11 @@
 <script setup>
-import { getCheckInfoAPI } from '@/apis/checkout'
+import { getCheckInfoAPI, createOrderAPI } from '@/apis/checkout'
 import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useCartStore } from '@/stores/cartStore'
 const curAddress = ref({})
+const router = useRouter()
+const CartStore = useCartStore()
 
 const checkInfo = ref({}) // 订单对象
 const getCheckInfo = async () => {
@@ -28,6 +32,32 @@ const switchAddress = (item) => {
 const confirm = () => {
   curAddress.value = activeAddress.value
   showDialog.value = false
+}
+
+//create order
+const createOrder = async () => {
+  const res = await createOrderAPI({
+    deliveryTimeType: 1,
+    payType: 1,
+    payChannel: 1,
+    buyerMessage: '',
+    goods: checkInfo.value.goods.map((item) => {
+      return {
+        skuId: item.skuId,
+        count: item.count,
+      }
+    }),
+    addressId: curAddress.value.id,
+  })
+  const orderId = res.result.id
+  router.push({
+    path: '/pay',
+    query: {
+      id: orderId,
+    },
+  })
+  //update cart list
+  CartStore.updateNewList()
 }
 </script>
 <template>
@@ -126,7 +156,7 @@ const confirm = () => {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large">提交订单</el-button>
+          <el-button type="primary" size="large" @click="createOrder">提交订单</el-button>
         </div>
       </div>
     </div>
